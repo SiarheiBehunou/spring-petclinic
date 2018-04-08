31ed9bb7a07563467f67256c5b98258e3460c377
Siarhei Behunou <siarhei_behunou@epam.com>
2018-04-09 00:50:47 +0300

    changes

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -26,8 +26,6 @@ import javax.persistence.MappedSuperclass;
  * Simple JavaBean domain object with an id property. Used as a base class for objects
  * needing this property.
  *
- * @author Ken Krebs
- * @author Juergen Hoeller
  */
 @MappedSuperclass
 public class BaseEntity implements Serializable {
